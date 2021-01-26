7c63d685c2a5a9cc331744aabd5935bd1d5221f2
nacardozog <nacardozog@heon.com.co>
2021-01-26 06:35:13 -0500

    Cargue inicial del proyecto

---
+++ Roulette/Roulette/Controllers/NewRouletteController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roulette.Controllers
+{
+    public class NewRouletteController
+    {
+    }
+}
