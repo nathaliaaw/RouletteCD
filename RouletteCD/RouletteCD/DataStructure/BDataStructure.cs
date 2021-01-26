e94512a536044fe876f9726adec90b35af9a9f27
nacardozog <nacardozog@heon.com.co>
2021-01-26 17:33:15 -0500

    Data type of the colorBet variable is changed.

--- RouletteCD/RouletteCD/DataStructure/BDataStructure.cs
+++ RouletteCD/RouletteCD/DataStructure/BDataStructure.cs
@@ -8,7 +8,7 @@ namespace RouletteCD.DataStructure
         public int? numberBet { get; set; }
         [Range(0.5d, maximum: 10000)]
         public int? moneyBet { get; set; }
-        public bool? colorBet { get; set; }
+        public string? colorBet { get; set; }
         public string? UserId { get; set; }
         
     }
